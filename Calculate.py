d78164df7f7c0bca6f6cd9aba211dfcd077d4865
Ken Felder <ken.felder@student.tuwien.ac.at>
2024-04-08 14:14:30 +0200

    reformated to pass test

--- Calculate.py
+++ Calculate.py
@@ -1,11 +1,13 @@
-#multiplies two numbers
+# multiplies two numbers
 def multiply(a, b):
     return a * b
 
-#divides first input by second input
+
+# divides first input by second input
 def divide(a, b):
     return a / b
 
-#returns the square of an input
+
+# returns the square of an input
 def square(a):
     return a ** 2
