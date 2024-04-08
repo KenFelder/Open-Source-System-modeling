0a6012b30dec65d082da2aa5d39bad8f9f4edea3
Ken Felder <ken.felder@student.tuwien.ac.at>
2024-04-08 14:25:53 +0200

    make all tests fail

--- test_Calculate.py
+++ test_Calculate.py
@@ -1,19 +1,16 @@
 import Calculate
 
-
 def test_multiply():
     assert Calculate.multiply(2, 2) == 4
     assert Calculate.multiply(1, 2) == 2
     assert Calculate.multiply(5, 3) == 15
-
 
 def test_divide():
     assert Calculate.divide(2, 2) == 1
     assert Calculate.divide(1, 2) == 0.5
     assert Calculate.divide(6, 3) == 2
 
-
 def test_square():
     assert Calculate.square(5) == 25
-    assert Calculate.square(1) == 1
+    assert Calculate.square(3) == 1
     assert Calculate.square(-6) == 36
