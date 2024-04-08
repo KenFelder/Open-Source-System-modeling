953e0a69de05a28d52600be8d73234043e23c237
Ken Felder <ken.felder@student.tuwien.ac.at>
2024-04-08 13:41:36 +0200

    added documentation

--- Calculate.py
+++ Calculate.py
@@ -1,14 +1,19 @@
+#multiplies two numbers
 def multiply(a, b):
     return a * b
 
+#adds two numbers
 def add(a, b):
     return a + b
 
+#subtracts second input from first input
 def subtract(a, b):
     return a - b
 
+#divides first input by second input
 def divide(a, b):
     return a / b
 
+#returns the square of an input
 def square(a):
     return a ** 2
